ec53585cc6d8f219a8a320bdf0bc74470cb594d3
omer6000 <63969246+omer6000@users.noreply.github.com>
2020-10-09 19:45:39 +0500

    added car movement

---
+++ Assets/Car.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Car : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+    
+    void ProcessInput() 
+    {
+        if(Input.GetKey(KeyCode.D))
+        {
+            rigidbody.AddRelativeForce(Vector3.up);
+        }
+        else if(Input.GetKey(KeyCode.A))
+        {
+            rigidbody.AddRelativeForce(Vector3.down);
+        }
+        else if(Input.GetKey(KeyCode.W))
+        {
+            rigidbody.AddRelativeForce(Vector3.right);
+        }
+        else if(Input.GetKey(KeyCode.S))
+        {
+            rigidbody.AddRelativeForce(Vector3.left);
+        }
+    }
+}

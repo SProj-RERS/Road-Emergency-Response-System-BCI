2273c103d9443c19a6e6ec6715fddce23549f887
omer6000 <63969246+omer6000@users.noreply.github.com>
2020-10-19 21:35:31 +0500

    Revert "Organizing Project into New Files"
    This reverts commit 9ff56e26bb4aaec5d295a2c816e3ba9f9a6d4668.

---
+++ Assets/MainCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+    void ProcessInput() 
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward);
+        }
+        else if(Input.GetKey(KeyCode.S))
+        {
+            GetComponent<Rigidbody>().AddRelativeForce(Vector3.back);
+        }
+    }
+}

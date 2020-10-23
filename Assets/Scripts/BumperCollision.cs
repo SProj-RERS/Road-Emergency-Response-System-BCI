40eda5c0881cf236b08cf2f919ef1f2fc701cbb3
omer6000 <63969246+omer6000@users.noreply.github.com>
2020-10-24 00:54:56 +0500

    ball

--- Assets/Scripts/BumperCollision.cs
+++ Assets/Scripts/BumperCollision.cs
@@ -5,12 +5,24 @@ using UnityEngine;
 public class BumperCollision : MonoBehaviour
 {
     void OnTriggerEnter(Collider other)
-    {       
-        if(other.name == "Obstacle")
+    {
+        // print("helloooo");       
+        if(other.tag == "othercar")
         {
             float xRange = 2f;
             float new_x = transform.localPosition.x + 0.1f;
             transform.localPosition = new Vector3(new_x,transform.localPosition.y,transform.localPosition.z);
         }
     }
+    // void OnCollisionEnter(Collision collision)
+    // {
+    //     print("outside if");
+    //     if(collision.collider.tag == "othercar")
+    //     {
+    //         print("inside if");
+    //         float xRange = 2f;
+    //         float new_x = transform.localPosition.x + 0.1f;
+    //         collision.transform.localPosition = new Vector3(new_x,transform.localPosition.y,transform.localPosition.z);
+    //     }
+    // }
 }
--- Assets/Scripts/HoodCollision.cs
+++ Assets/Scripts/HoodCollision.cs
@@ -6,9 +6,18 @@ public class HoodCollision : MonoBehaviour
 {
     void OnTriggerEnter(Collider other)
     {       
-        if(other.name == "Obstacle")
+        if(other.tag == "othercar")
         {
             transform.localRotation = Quaternion.Euler(-160f,0f,0f);
         }
     }
+    // void OnCollisionEnter(Collision collision)
+    // {
+    //     print("outside if");
+    //     if(collision.collider.tag == "othercar")
+    //     {
+    //         print("inside if");
+    //         collision.transform.localRotation = Quaternion.Euler(-160f,0f,0f);
+    //     }
+    // }
 }

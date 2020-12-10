9d7d2480ecdf21ebd3110a3ad067b8d87d687f2e
omer6000 <63969246+omer6000@users.noreply.github.com>
2020-12-11 04:20:33 +0500

    making ambulance invisible

---
+++ Assets/Scripts/ambulance.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ambulance : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Renderer mr;
+    void Start()
+    {
+        mr = GetComponent<Renderer>();
+        // print(mr);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

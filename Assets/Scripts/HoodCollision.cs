f49fe40c3d773d49859b663dbf5364080555b718
Momin Mehmood Butt <butt.m.momin@gmail.com>
2020-12-16 17:06:04 +0500

    hehe

--- Assets/Scripts/HoodCollision.cs
+++ Assets/Scripts/HoodCollision.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class HoodCollision : MonoBehaviour
 {
-   public ParticleSystem explosionParticle;
+   public ParticleSystem explosionSmoke;
+   public ParticleSystem explosionFire;
    public AudioClip crashSound;
    public AudioClip thudSound;
+   public int hit_count = 0;
    private AudioSource carAudio;
 
    void Start()
    {
-       explosionParticle.Stop();
+       explosionSmoke.Stop();
+       explosionFire.Stop();
        carAudio = GetComponent<AudioSource>();
    }
 
@@ -19,16 +22,23 @@ public class HoodCollision : MonoBehaviour
     {       
         if(other.tag == "othercar" || other.tag == "streetlight")
         {
+            if(hit_count == 8)
+            {
+                explosionFire.Play();
+            }
+            hit_count += 1;
+            print(hit_count);
             transform.localRotation = Quaternion.Euler(-28.961f,0.002f,0f);
             float new_y = transform.localPosition.y - 0.51f;
             float new_z = transform.localPosition.z + 0.45f;
             transform.localPosition = new Vector3(0f,new_y,new_z);
-            explosionParticle.Play();
-            carAudio.PlayOneShot(crashSound, 1.0f);  
+            explosionSmoke.Play();
+            carAudio.PlayOneShot(crashSound, 1.0f);
         }
         if(other.tag == "footpath")
         {
             carAudio.PlayOneShot(thudSound, 1.0f);
         }
     }
+
 }

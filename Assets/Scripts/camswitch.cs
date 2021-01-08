b5fe609dc08c2f30728b6b113df7d088cc7644ed
Mehreen Masood <21100048@lums.edu.pk>
2021-01-09 00:03:16 +0500

    blood fix

---
+++ Assets/Scripts/camswitch.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camswitch : MonoBehaviour
+{
+    public Camera FpsCam;
+    public Camera TpsCam;
+    bool fpsswitch = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        FpsCam.enabled = fpsswitch;
+        TpsCam.enabled = !fpsswitch;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetKeyDown(KeyCode.G)){
+           fpsswitch = !fpsswitch;
+           FpsCam.enabled = fpsswitch;
+           TpsCam.enabled = !fpsswitch;
+       } 
+    }
+}

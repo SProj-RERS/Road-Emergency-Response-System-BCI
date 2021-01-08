a92faed8132b3586ca7d74697e1b774f1093077b
Mehreen Masood <21100048@lums.edu.pk>
2021-01-09 02:53:16 +0500

    loadscene

--- Assets/Scripts/LoadGame.cs
+++ Assets/Scripts/LoadGame.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadGame : MonoBehaviour
 {
+    private AssetBundle myLoadedAssetBundle;
+    private string[] scenePaths;
+
     public void changemenuscene(string scenename)
     {
-        Application.LoadLevel(scenename);
+       SceneManager.LoadScene(scenename,LoadSceneMode.Single);
     }
 }
